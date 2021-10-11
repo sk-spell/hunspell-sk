d6fe628c38b658662e14c57e7ae757e8e0ce8fda
zdenop <zdenop@gmail.com>
2021-10-11 19:59:47 +0200

    improve build.bat

--- build.bat
+++ build.bat
@@ -16,26 +16,27 @@ REM BUILD Process
 mkdir build
 copy /y NUL build\dict >NUL
 
-%sed% 1,1d _osobnosti\sport.dic >>build\dict
-%sed% 1,1d _osobnosti\politika.dic >>build\dict
-%sed% 1,1d _osobnosti\it.dic >>build\dict
-%sed% 1,1d _skratky\it.dic >>build\dict
-%sed% 1,1d _skratky\politika.dic >>build\dict
-%sed% 1,1d _tematicke\nabozenske.dic >>build\dict
-%sed% 1,1d _tematicke\cudzie.dic >>build\dict
-%sed% 1,1d _terminy\it.dic >>build\dict
-%sed% 1,1d sk_SK.dic >>build\dict
+%sed% 1,1d _osobnosti/sport.dic >>build/dict
+%sed% 1,1d _osobnosti/politika.dic >>build/dict
+%sed% 1,1d _osobnosti/it.dic >>build/dict
+%sed% 1,1d _skratky/it.dic >>build/dict
+%sed% 1,1d _skratky/politika.dic >>build/dict
+%sed% 1,1d _tematicke/nabozenske.dic >>build/dict
+%sed% 1,1d _tematicke/cudzie.dic >>build/dict
+%sed% 1,1d _terminy/it.dic >>build/dict
+%sed% 1,1d sk_SK.dic >>build/dict
 
-%sort% -u build\dict > build\temp.dic
-%grep% "/" build\temp.dic >build\sk_flag.dic
-%grep% -v "/" build\temp.dic | %grep% ":" >build\sk_noflag_pos.dic
-%grep% -v "/" build\temp.dic | %grep% -v ":" >build\sk_noflag.dic
-copy sk_SK.aff >>build\sk_flag.aff
+%sort% -u build/dict > build/temp.dic
+%grep% "/" build/temp.dic >build/sk_fl.dic
+%grep% -v "/" build/temp.dic | %grep% ":" >build/sk_noflag_pos.dic
+%grep% -v "/" build/temp.dic | %grep% -v ":" >build/sk_noflag.dic
+copy sk_SK.aff build\sk_fl.aff
 
-%hunspell% -d build\sk_flag -l build\sk_noflag.dic >build\add.words
-%dos2unix% build\add.words
-%cat% build\sk_noflag_pos.dic build\add.words build\sk_flag.dic | %sort% -u >build\sk_SK.dic
-%wc% -l < build\sk_SK.dic | cat - build\sk_SK.dic >sk_SK.dic
+%hunspell% -d build/sk_fl -l build/sk_noflag.dic >build/add.words
+%dos2unix% build/add.words
+%cat% build/sk_noflag_pos.dic build/add.words build/sk_fl.dic > build/temp.dic
+%sort% -u <build/temp.dic >build/sk_SK.dic
+%wc% -l < build/sk_SK.dic | cat - build/sk_SK.dic >sk_SK.new.dic.
 
 REM Clean up
 REM rmdir /s/q build
